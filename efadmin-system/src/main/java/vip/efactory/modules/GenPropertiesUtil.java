3328e681ddf900eb095934ed16950b5dec8b62db
dbdu <1797890817@qq.com>
2020-01-12 13:30:20 +0800

    增加遗漏的国际化实体

--- efadmin-system/src/main/java/vip/efactory/modules/GenPropertiesUtil.java
+++ efadmin-system/src/main/java/vip/efactory/modules/GenPropertiesUtil.java
@@ -1,10 +1,14 @@
 package vip.efactory.modules;
 
-import cn.hutool.core.lang.Dict;
 import vip.efactory.common.i18n.enums.Entityi18nUtil;
 
 import vip.efactory.domain.*;
 import vip.efactory.ejpa.base.entity.BaseEntity;
+import vip.efactory.modules.mnt.domain.*;
+import vip.efactory.modules.monitor.domain.Server;
+import vip.efactory.modules.monitor.domain.Visits;
+import vip.efactory.modules.quartz.domain.QuartzJob;
+import vip.efactory.modules.quartz.domain.QuartzLog;
 import vip.efactory.modules.system.domain.*;
 
 import java.util.ArrayList;
@@ -42,6 +46,18 @@ public class GenPropertiesUtil {
         classes.add(Role.class);
         classes.add(User.class);
         classes.add(UserAvatar.class);
+        // 定时任务
+        classes.add(QuartzJob.class);
+        classes.add(QuartzLog.class);
+        // 监控部分
+        classes.add(Server.class);
+        classes.add(Visits.class);
+        // 部署管理部分
+        classes.add(App.class);
+        classes.add(Database.class);
+        classes.add(Deploy.class);
+        classes.add(DeployHistory.class);
+        classes.add(ServerDeploy.class);
 
         Entityi18nUtil.copyToLocale(Entityi18nUtil.geni18nPropertiesFile("messages", classes));
     }
