65a53b33cbcc32c0995c10a26a8a5b7e592ab248
dbdu <1797890817@qq.com>
2020-01-29 13:40:58 +0800

    增加监控模块

---
+++ efadmin-monitor/src/main/java/vip/efactory/MonitorRun.java
@@ -0,0 +1,15 @@
+package me.zhengjie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class MonitorRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorRun.class, args);
+    }
+
+}
