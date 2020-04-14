ff82a48baaf4c7e8e701045736ffcedfedf4d79d
dbdu <1797890817@qq.com>
2020-04-14 20:57:18 +0800

    修正多租户模式下访问统计可能空指针的问题

--- efadmin-system/src/main/java/vip/efactory/modules/monitor/service/impl/VisitsServiceImpl.java
+++ efadmin-system/src/main/java/vip/efactory/modules/monitor/service/impl/VisitsServiceImpl.java
@@ -1,22 +1,24 @@
 package vip.efactory.modules.monitor.service.impl;
 
-import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import lombok.extern.slf4j.Slf4j;
 import vip.efactory.ejpa.base.service.impl.BaseServiceImpl;
 import vip.efactory.modules.monitor.domain.Visits;
 import vip.efactory.modules.monitor.repository.VisitsRepository;
 import vip.efactory.modules.monitor.service.VisitsService;
 import vip.efactory.repository.LogRepository;
 import vip.efactory.utils.StringUtils;
-
-import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -25,7 +27,7 @@ public class VisitsServiceImpl extends BaseServiceImpl<Visits, Long, VisitsRepos
 
     private final LogRepository logRepository;
 
-    public VisitsServiceImpl( LogRepository logRepository) {
+    public VisitsServiceImpl(LogRepository logRepository) {
         this.logRepository = logRepository;
     }
 
@@ -47,15 +49,24 @@ public class VisitsServiceImpl extends BaseServiceImpl<Visits, Long, VisitsRepos
     public void count(HttpServletRequest request) {
         LocalDate localDate = LocalDate.now();
         Visits visits = br.findByDate(localDate.toString());
-        visits.setPvCounts(visits.getPvCounts() + 1);
-        long ipCounts = logRepository.findIp(localDate.toString(), localDate.plusDays(1).toString());
-        visits.setIpCounts(ipCounts);
-        br.save(visits);
+        if (visits == null) {
+            visits = new Visits();
+            visits.setWeekDay(StringUtils.getWeekDay());
+            visits.setPvCounts(1L);
+            visits.setIpCounts(1L);
+            visits.setDate(localDate.toString());
+            br.save(visits);
+        } else {
+            visits.setPvCounts(visits.getPvCounts() + 1);
+            long ipCounts = logRepository.findIp(localDate.toString(), localDate.plusDays(1).toString());
+            visits.setIpCounts(ipCounts);
+            br.save(visits);
+        }
     }
 
     @Override
     public Object get() {
-        Map<String,Object> map = new HashMap<>(4);
+        Map<String, Object> map = new HashMap<>(4);
         LocalDate localDate = LocalDate.now();
         Visits visits = br.findByDate(localDate.toString());
         List<Visits> list = br.findAllVisits(localDate.minusDays(6).toString(), localDate.plusDays(1).toString());
@@ -65,8 +76,14 @@ public class VisitsServiceImpl extends BaseServiceImpl<Visits, Long, VisitsRepos
             recentVisits += data.getPvCounts();
             recentIp += data.getIpCounts();
         }
-        map.put("newVisits", visits.getPvCounts());
-        map.put("newIp", visits.getIpCounts());
+
+        if (visits == null) {
+            map.put("newVisits", 0L);
+            map.put("newIp", 0L);
+        } else {
+            map.put("newVisits", visits.getPvCounts());
+            map.put("newIp", visits.getIpCounts());
+        }
         map.put("recentVisits", recentVisits);
         map.put("recentIp", recentIp);
         return map;
@@ -74,7 +91,7 @@ public class VisitsServiceImpl extends BaseServiceImpl<Visits, Long, VisitsRepos
 
     @Override
     public Object getChartData() {
-        Map<String,Object> map = new HashMap<>(3);
+        Map<String, Object> map = new HashMap<>(3);
         LocalDate localDate = LocalDate.now();
         List<Visits> list = br.findAllVisits(localDate.minusDays(6).toString(), localDate.plusDays(1).toString());
         map.put("weekDays", list.stream().map(Visits::getWeekDay).collect(Collectors.toList()));
